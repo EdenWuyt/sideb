ddeb4a296a146813e9bf81f03cf6bb4c17f599e7
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2026-06-21 19:55:06 +0800

    Implement header and add assets.

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+    id: number;
+    name: string;
+    artist: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    genre: string;
+    label: string;
+    stock: number;
+}
